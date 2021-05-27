a399c65600f81fde1c1226ffb146118f7032f4d7
ioiono <18642966+ioiono@users.noreply.github.com>
2021-05-28 03:35:15 +0800

    feat: 1st version

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # VPS-Setup
+
+## Usage 
+*Format is borrowed from [ohmyzsh](https://github.com/ohmyzsh/ohmyzsh). :)*
+### Basic Installation
+
+| Method    | Command                                                                                           |
+|:----------|:--------------------------------------------------------------------------------------------------|
+| **curl**  | `sh -c "$(curl -fsSL https://raw.githubusercontent.com/ioiono/VPS-Setup/master/src/init.sh)"` |
+| **wget**  | `sh -c "$(wget -O- https://raw.githubusercontent.com/ioiono/VPS-Setup/master/src/init.sh)"`   |
+| **fetch** | `sh -c "$(fetch -o - https://raw.githubusercontent.com/ioiono/VPS-Setup/master/src/init.sh)"` |
+
+#### Manual inspection
+
+It's a good idea to inspect the install script from projects you don't yet know. You can do
+that by downloading the install script first, looking through it so everything looks normal,
+then running it:
+
+```shell
+wget https://raw.githubusercontent.com/ioiono/VPS-Setup/master/src/init.sh -O install.sh
+sh install.sh
+```
---
+++ src/init.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#
+# This script should be run via curl:
+#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ioiono/VPS-Setup/master/src/init.sh)"
+# or via wget:
+#   sh -c "$(wget -qO- https://raw.githubusercontent.com/ioiono/VPS-Setup/master/src/init.sh)"
+# or via fetch:
+#   sh -c "$(fetch -o - https://raw.githubusercontent.com/ioiono/VPS-Setup/master/src/init.sh)"
+#
+# As an alternative, you can first download the install script and run it afterwards:
+#   wget https://raw.githubusercontent.com/ioiono/VPS-Setup/master/src/init.sh -O install.sh
+#   sh install.sh
+
+set -e
+
+timedatectl set-timezone Asia/Singapore
+
+apt-get update && apt-get install -y \
+    curl \
+    git \
+    zsh \
+    wget \
+    sudo \
+    vim \
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+chsh -s "$(which zsh)"
+
+
+# change default zsh theme
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="crcandy"/' ~/.zshrc
+sed -i 's/^plugins=.*/plugins=(git docker docker-compose zsh-syntax-highlighting zsh-autosuggestions zsh-completions timer)/' ~/.zshrc
+
+# zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}"/plugins/zsh-completions
+
+{
+echo "autoload -U compinit && compinit"
+echo "ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=\"fg=7\""
+echo "export TIMER_PRECISION=2"
+# shellcheck disable=SC2028
+echo "export TIMER_FORMAT=\"\n\$fg[white]Time: %d\""
+} >> ~/.zshrc
+
+# start using zsh
+
+zsh
