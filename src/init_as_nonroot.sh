4d464eddb1dd4929bb1b1867206ebddc98d61a3d
ioiono <18642966+ioiono@users.noreply.github.com>
2021-05-30 20:59:02 +0800

    feat: opt

---
+++ src/init_as_nonroot.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+#
+# This script should be run via curl:
+#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ioiono/VPS-Setup/master/src/init_as_nonroot.sh)"
+# or via wget:
+#   sh -c "$(wget -qO- https://raw.githubusercontent.com/ioiono/VPS-Setup/master/src/init_as_nonroot.sh)"
+# or via fetch:
+#   sh -c "$(fetch -o - https://raw.githubusercontent.com/ioiono/VPS-Setup/master/src/init_as_nonroot.sh)"
+#
+# As an alternative, you can first download the install script and run it afterwards:
+#   wget https://raw.githubusercontent.com/ioiono/VPS-Setup/master/src/init_as_nonroot.sh -O install.sh
+#   sh install.sh
+
+set -e
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+timedatectl set-timezone Asia/Singapore
+
+apt-get update && apt-get install -y \
+    curl \
+    git \
+    zsh \
+    wget \
+    sudo \
+    vim \
+
+# shellcheck disable=SC2059
+printf "\n${GREEN}Installation done.${NC}\n\n"
+
+# run after switch user
+useradd -rm -d /home/admin -s /bin/zsh -g root -G sudo -u 1000 -p "$(openssl passwd -1 admin)" admin
+
+printf "\n${GREEN}admin created.${NC}\n\n"
+
+
+# now run command as admin
+whoami
+sudo -i -u admin bash << EOF
+echo "In"
+whoami
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+chsh -s "$(which zsh)"
+
+
+# change default zsh theme
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="crcandy"/' ~/.zshrc
+sed -i 's/^plugins=.*/plugins=(git docker docker-compose zsh-syntax-highlighting zsh-autosuggestions zsh-completions timer)/' ~/.zshrc
+
+# zsh plugins
+# shellcheck disable=SC2086
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# shellcheck disable=SC2086
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# shellcheck disable=SC2086
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+{
+echo "autoload -U compinit && compinit"
+echo "ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=\"fg=7\""
+echo "export TIMER_PRECISION=2"
+# shellcheck disable=SC2028
+echo "export TIMER_FORMAT=\"\n\$fg[white]Time: %d\""
+} >> ~/.zshrc
+
+EOF
+echo "Out"
+whoami
+
+printf "\n${GREEN}Finished.${NC}\n\n"
