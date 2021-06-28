0bb94f030e66b9feb26fe44a9770aa9806737255
ioiono <18642966+ioiono@users.noreply.github.com>
2021-06-28 23:35:12 +0800

    feat: updated

--- src/init_as_nonroot.sh
+++ src/init_as_nonroot.sh
@@ -49,13 +49,14 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # change default zsh theme
 sed -i 's/^ZSH_THEME=.*/ZSH_THEME="crcandy"/' ~/.zshrc
-sed -i 's/^plugins=.*/plugins=(git brew docker docker-compose zsh-syntax-highlighting zsh-autosuggestions zsh-completions)/' ~/.zshrc
+sed -i 's/^plugins=.*/plugins=(git brew docker docker-compose zsh-syntax-highlighting zsh-autosuggestions zsh-completions command-time)/' ~/.zshrc
 
 # zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/popstas/zsh-command-time.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/command-time
+
 {
 echo "autoload -U compinit && compinit"
 echo "ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=\"fg=7\""
